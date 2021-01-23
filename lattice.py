02e853a360c9fa8bfbd788c74c638496670b2424
Matthew <butcher@rice.edu>
2021-01-23 11:34:47 -0600

    Decide on Lattice construction.

--- lattice.py
+++ lattice.py
@@ -1,18 +1,21 @@
 import numpy as np
 
+
+def get_lattice_geometry(lattice_type, basis=):
+    if lattice_type == 'triangle':
+
+
 class Lattice:
     def __init__(self, lat_type, lx, ly, unit_cell_mult=1):
         if lat_type == 'triangle':
             x = np.array([1.0, 0.0])
             self.basis = x[:, None] * np.arange(unit_cell_mult)[None, :]
-            basemat = np.dstack([self.basis]*lx*ly).transpose((0, 2, 1)).reshape((2, lx*ly*unit_cell_mult))
-            print(basemat)
-
             self.a1 = x*unit_cell_mult
             self.a2 = np.array([0.5, 0.5*np.sqrt(3)])
-            xmat = np.concatenate([np.arange(lx)]*ly).reshape((ly, 2)).transpose()
-            ymat = np.dstack([np.arange(ly)]*lx).transpose()
-            print(xmat)
-            print(ymat)
+            rlist = []
+            for i in np.arange(lx):
+                for j in np.arange(ly):
+                    for b in np.arange(unit_cell_mult):
+                        rlist.append(self.a1*i + self.a2*j + self.basis[:, b])
 
-            # self.coordinates = np.repeat(self.a1[np.newaxis, :] * i, unit_cell_mult, axis=0) + self.basis
+            self.coordinates = np.array(rlist).transpose()
